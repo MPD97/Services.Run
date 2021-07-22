952099d7890ed521a2b935959be1abad41f29cf4
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-22 14:41:36 +0200

    Created queries

---
+++ src/Services.Run.Application/Queries/GetRun.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Convey.CQRS.Queries;
+using Services.Run.Application.DTO;
+
+namespace Services.Run.Application.Queries
+{
+    public class GetRun: IQuery<RunDto>
+    {
+        public Guid RunId { get; set; }
+    }
+}
---
+++ src/Services.Run.Application/Queries/GetRuns.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Convey.CQRS.Queries;
+using Services.Run.Application.DTO;
+
+namespace Services.Run.Application.Queries
+{
+    public class GetRuns : IQuery<IEnumerable<RunDto>>
+    {
+    }
+}
