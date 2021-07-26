3238aa347dda73d31d2450a89846cc63d492589d
MPD97 <mateuszpracadom@gmail.com>
2021-07-26 14:36:39 +0200

    Updated extension

--- src/Services.Run.Infrastructure/Extensions.cs
+++ src/Services.Run.Infrastructure/Extensions.cs
@@ -1,12 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Convey;
+using Convey.CQRS.Commands;
+using Convey.CQRS.Events;
+using Convey.CQRS.Queries;
+using Convey.Discovery.Consul;
+using Convey.Docs.Swagger;
+using Convey.HTTP;
+using Convey.LoadBalancing.Fabio;
 using Convey.MessageBrokers;
+using Convey.MessageBrokers.CQRS;
+using Convey.MessageBrokers.Outbox;
+using Convey.MessageBrokers.Outbox.Mongo;
+using Convey.MessageBrokers.RabbitMQ;
+using Convey.Metrics.AppMetrics;
+using Convey.Persistence.MongoDB;
+using Convey.Persistence.Redis;
+using Convey.Security;
+using Convey.Tracing.Jaeger;
+using Convey.Tracing.Jaeger.RabbitMQ;
+using Convey.WebApi;
+using Convey.WebApi.CQRS;
+using Convey.WebApi.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
+using Services.Run.Application;
+using Services.Run.Application.Commands;
+using Services.Run.Application.Events.External;
+using Services.Run.Application.Services;
+using Services.Run.Core.Repositories;
 using Services.Run.Infrastructure.Contexts;
+using Services.Run.Infrastructure.Decorators;
+using Services.Run.Infrastructure.Exceptions;
+using Services.Run.Infrastructure.Logging;
+using Services.Run.Infrastructure.Mongo.Documents;
+using Services.Run.Infrastructure.Mongo.Repositories;
+using Services.Run.Infrastructure.Services;
 
 namespace Services.Run.Infrastructure
 {
@@ -14,11 +47,53 @@ namespace Services.Run.Infrastructure
     {
         public static IConveyBuilder AddInfrastructure(this IConveyBuilder builder)
         {
-            return builder;
+            builder.Services.AddSingleton<IEventMapper, EventMapper>();
+            builder.Services.AddTransient<IMessageBroker, MessageBroker>();
+            builder.Services.AddTransient<IUserRepository, UserMongoRepository>();
+            builder.Services.AddTransient<IRunRepository, RunMongoRepository>();
+            builder.Services.AddTransient<IUserRepository, UserMongoRepository>();
+            builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+            builder.Services.AddSingleton<IDistanceMeasure, DistanceMesure>();
+            builder.Services.AddTransient<IAppContextFactory, AppContextFactory>();
+            builder.Services.AddTransient(ctx => ctx.GetRequiredService<IAppContextFactory>().Create());
+            builder.Services.TryDecorate(typeof(ICommandHandler<>), typeof(OutboxCommandHandlerDecorator<>));
+            builder.Services.TryDecorate(typeof(IEventHandler<>), typeof(OutboxEventHandlerDecorator<>));
+            
+            return builder
+                .AddErrorHandler<ExceptionToResponseMapper>()
+                .AddQueryHandlers()
+                .AddInMemoryQueryDispatcher()
+                .AddHttpClient()
+                .AddConsul()
+                .AddFabio()
+                .AddRabbitMq(plugins: p => p.AddJaegerRabbitMqPlugin())
+                .AddMessageOutbox(o => o.AddMongo())
+                .AddExceptionToMessageMapper<ExceptionToMessageMapper>()
+                .AddMongo()
+                .AddRedis()
+                .AddMetrics()
+                .AddJaeger()
+                .AddHandlersLogging()
+                .AddMongoRepository<UserDocument, Guid>("users")
+                .AddMongoRepository<RunDocument, Guid>("runs")
+                .AddWebApiSwaggerDocs()
+                .AddSecurity();
         }
 
         public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
         {
+            app.UseErrorHandler()
+                .UseSwaggerDocs()
+                .UseJaeger()
+                .UseConvey()
+                .UsePublicContracts<ContractAttribute>()
+                .UseMetrics()
+                .UseRabbitMq()
+                .SubscribeCommand<CreateRun>()
+                .SubscribeEvent<UserCreated>()
+                .SubscribeEvent<UserStateChanged>()
+                .SubscribeEvent<LocationAdded>();
+            
             return app;         
         }  
         internal static CorrelationContext GetCorrelationContext(this IHttpContextAccessor accessor)
