2b5a1600641c076e14bf14fbc54495747cc87ae5
MPD97 <mateuszpracadom@gmail.com>
2021-07-26 14:36:24 +0200

    Created queries

---
+++ src/Services.Run.Infrastructure/Mongo/Queries/Handlers/GetRunHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.CQRS.Queries;
+using Convey.Persistence.MongoDB;
+using Services.Run.Application.DTO;
+using Services.Run.Application.Queries;
+using Services.Run.Infrastructure.Mongo.Documents;
+
+namespace Services.Run.Infrastructure.Mongo.Queries.Handlers
+{
+    public class GetRunHandler: IQueryHandler<GetRun, RunDto>
+    {
+        private readonly IMongoRepository<RunDocument, Guid> _runDocument;
+
+        public GetRunHandler(IMongoRepository<RunDocument, Guid> runDocument)
+        {
+            _runDocument = runDocument;
+        }
+
+        public async Task<RunDto> HandleAsync(GetRun query)
+        {
+            var document = await _runDocument.GetAsync(p => p.Id == query.RunId);
+
+            return document?.AsDto();
+        }
+    }
+}
---
+++ src/Services.Run.Infrastructure/Mongo/Queries/Handlers/GetRunsHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Convey.CQRS.Queries;
+using Convey.Persistence.MongoDB;
+using Services.Run.Application.DTO;
+using Services.Run.Application.Queries;
+using Services.Run.Infrastructure.Mongo.Documents;
+
+namespace Services.Run.Infrastructure.Mongo.Queries.Handlers
+{
+    public class GetRunsHandler: IQueryHandler<GetRuns, IEnumerable<RunDto>>
+    {
+        private readonly IMongoRepository<RunDocument, Guid> _runDocument;
+
+        public GetRunsHandler(IMongoRepository<RunDocument, Guid> runDocument)
+        {
+            _runDocument = runDocument;
+        }
+
+
+        public async Task<IEnumerable<RunDto>> HandleAsync(GetRuns query)
+        {
+            var routes = await _runDocument.FindAsync(_ => true);
+
+            return routes.Select(r => Documents.Extensions.AsDto(r));
+        }
+    }
+}
