64a5abfa64fa0a4f7035f7a2e7f1baf2272f0d3d
MPD97 <mateuszpracadom@gmail.com>
2021-07-26 14:35:31 +0200

    Created repositories

---
+++ src/Services.Run.Infrastructure/Mongo/Repositories/RunMongoRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.Persistence.MongoDB;
+using Services.Run.Core.Entities;
+using Services.Run.Core.Repositories;
+using Services.Run.Infrastructure.Mongo.Documents;
+
+namespace Services.Run.Infrastructure.Mongo.Repositories
+{
+    public class RunMongoRepository : IRunRepository
+    {
+        private readonly IMongoRepository<RunDocument, Guid> _repository;
+
+        public RunMongoRepository(IMongoRepository<RunDocument, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Core.Entities.Run> GetAsync(Guid id)
+        {
+            var run = await _repository.GetAsync(r => r.Id == id);
+    
+            return run?.AsEntity();
+        }
+
+        public async Task<Core.Entities.Run> GetActiveRunByUserId(Guid id)
+        {
+            var run = await _repository.GetAsync(r => r.Id == id && r.Status == Status.Started);
+    
+            return run?.AsEntity();        }
+
+        public async Task AddAsync(Core.Entities.Run run)
+            => await _repository.AddAsync(run.AsDocument());
+
+        public async Task UpdateAsync(Core.Entities.Run run)
+            => await _repository.UpdateAsync(run.AsDocument());
+    }
+}
---
+++ src/Services.Run.Infrastructure/Mongo/Repositories/UserMongoRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.Persistence.MongoDB;
+using Services.Run.Core.Entities;
+using Services.Run.Core.Repositories;
+using Services.Run.Infrastructure.Mongo.Documents;
+
+namespace Services.Run.Infrastructure.Mongo.Repositories
+{
+    public class UserMongoRepository : IUserRepository
+    {
+        private readonly IMongoRepository<UserDocument, Guid> _repository;
+
+        public UserMongoRepository(IMongoRepository<UserDocument, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> ExistsAsync(Guid id)
+            => await _repository.ExistsAsync(u => u.Id == id);
+
+        public async Task<User> GetAsync(Guid id)
+        {
+            var user = await _repository.GetAsync(u => u.Id == id);
+
+            return user?.AsEntity();
+        }
+
+        public Task AddAsync(User user) => _repository.AddAsync(user.AsDocument());
+        public Task UpdateAsync(User user) => _repository.UpdateAsync(user.AsDocument());
+    }
+}
