95fc587241beaa77633963f244773ae05d79bd48
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-27 14:42:51 +0200

    Created services

--- src/Services.Run.Application/Services/Route/IRouteServiceClient.cs
+++ src/Services.Run.Application/Services/Route/IRouteServiceClient.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using Services.Run.Application.DTO;
 
---
+++ src/Services.Run.Infrastructure/Services/Route/RouteServiceClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.HTTP;
+using Convey.Secrets.Vault;
+using Convey.WebApi.Security;
+using Services.Run.Application.DTO;
+using Services.Run.Application.Services.Route;
+
+namespace Services.Run.Infrastructure.Services.Route
+{
+    internal sealed class RouteServiceClient: IRouteServiceClient
+    {
+        private readonly IHttpClient _httpClient;
+        private readonly string _url;
+
+        public RouteServiceClient(IHttpClient httpClient, HttpClientOptions options)
+        {
+            _httpClient = httpClient;
+            _url = options.Services["route"];
+          
+        }
+
+        public Task<RouteDto> GetAsync(Guid id)
+            => _httpClient.GetAsync<RouteDto>($"{_url}/routes/{id}");
+    }
+}
